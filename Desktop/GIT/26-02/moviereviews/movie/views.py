10f55dffe99b6ee21a85856282d7cac3c14da7b4
Neelrajsinh Zala <159137288+neel-prowerse@users.noreply.github.com>
2024-02-26 16:49:22 +0530

    Adding Django Project

---
+++ Desktop/GIT/26-02/moviereviews/movie/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .models import Movie
+# Create your views here.
+def home(request):
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else: 
+        movies = Movie.objects.all()
+    return render(request,'home.html',{'searchTerm':searchTerm,'movies':movies})
+def signup(request):
+    email = request.GET.get('email')
+    return render(request,'signup.html',{'email':email})
