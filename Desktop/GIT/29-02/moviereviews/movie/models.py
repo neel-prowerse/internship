ea0ae0b95734dc69b74dc5b1392e3d78a5c6d773
Neelrajsinh Zala <159137288+neel-prowerse@users.noreply.github.com>
2024-02-29 16:45:41 +0530

    Revised Movie App

---
+++ Desktop/GIT/29-02/moviereviews/movie/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=500)
+    image = models.ImageField(upload_to='movie/images/')
+    url = models.URLField(blank=True)
+    def __str__(self):
+        return self.title
+class Review(models.Model):
+    text = models.CharField(max_length=100)
+    date = models.DateField(auto_now_add=True)
+    user = models.ForeignKey(Movie, on_delete = models.CASCADE)
+    watchAgain = models.BooleanField()
+    def __str__(self):
+        return self.text
